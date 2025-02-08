341cb2f47bc0fd0691b8241e4266a2f2f9249609
이신혁 <69188972+dawnfire05@users.noreply.github.com>
2025-02-08 23:28:38 +0900

    fit group model with group info response (#551)
    * fix: group model
    
    * feat: group user model now implements group user entity

--- lib/app/modules/groups/data/data_sources/models/group_user_model.dart
+++ lib/app/modules/groups/data/data_sources/models/group_user_model.dart
@@ -1,17 +1,17 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:ziggle/app/modules/user/domain/entities/user_entity.dart';
+import 'package:ziggle/app/modules/groups/domain/entities/group_user_entity.dart';
 
 part 'group_user_model.freezed.dart';
 part 'group_user_model.g.dart';
 
 @freezed
-class GroupUserModel with _$GroupUserModel implements UserEntity {
+class GroupUserModel with _$GroupUserModel implements GroupUserEntity {
   const GroupUserModel._();
   const factory GroupUserModel({
-    required String email,
-    required String name,
-    @JsonKey(name: 'studentNumber') required String studentId,
     required String uuid,
+    required String name,
+    required String email,
+    required DateTime createdAt,
   }) = _UserModel;
 
   factory GroupUserModel.fromJson(Map<String, dynamic> json) =>
